e12b0909e258d411360d5a09234d214304a14bbf
Jaddrrz <jaddrrz.javonillo@gmail.com>
2024-12-15 00:26:14 +0100

    write more pseudocode

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -2,9 +2,13 @@
 
 puts "_" + "|" + "_" + "|" + "_" ,  "_" + "|" + "_" + "|" + "_" , " " + "|" + " " + "|" + " "
 
-
-
-# Find a way to choose where to put either cross or circle
+# Find a way to choose where to put either cross or circle:
+# - make a "tile" class where you put a circle/cross
+# - find a way to put it in, maybe use positions like a1, b3, c2 
+# - think of making a board, line/diagonal/ and tile classes to manage everything:
+#   - board class is used as a container for tiles, maybe use to manage sign choices
+#   - line class check whether a player has won or not
+#   
 
 # Check if anyone has won or drawn
 
