fc8093ed036efb4cd50adea2bf9c9e2da6ddf220
Jaddrrz <jaddrrz.javonillo@gmail.com>
2024-12-15 00:18:27 +0100

    make board

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,9 @@
 # Make a tic tac toe board on the command line
 
+puts "_" + "|" + "_" + "|" + "_" ,  "_" + "|" + "_" + "|" + "_" , " " + "|" + " " + "|" + " "
+
+
+
 # Find a way to choose where to put either cross or circle
 
 # Check if anyone has won or drawn
