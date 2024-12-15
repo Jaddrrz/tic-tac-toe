eb703a60e2ec82c09bc8cf810734e3cff5d13db8
Jaddrrz <jaddrrz.javonillo@gmail.com>
2024-12-16 00:31:29 +0100

    Update pseudocode, add reference grid

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,15 +1,16 @@
-# Make a tic tac toe board on the command line
-
+# Create initial board with reference
 puts "_" + "|" + "_" + "|" + "_" ,  "_" + "|" + "_" + "|" + "_" , " " + "|" + " " + "|" + " "
+puts "Use these as reference" , "1 2 3", "4 5 6", "7 8 9"
 
-# Find a way to choose where to put either cross or circle:
+# Find a way to choose where to a mark:
 # - make a "tile" class where you put a circle/cross
 # - find a way to put it in, maybe use positions like a1, b3, c2 
-# - think of making a board, line/diagonal/ and tile classes to manage everything:
-#   - board class is used as a container for tiles, maybe use to manage sign choices
+# - think of making a grid, line/diagonal and tile classes to manage everything:
+#   - grid class is used as a container for tiles, maybe use to manage marks
 #   - line class check whether a player has won or not
-#   
 
-# Check if anyone has won or drawn
+
+# use range from 1 to 9 for tiles, a player chooses the corresponding tile number to locate mark, randomly decide which tile the npc chooses,
+# create a win condition, maybe insert all possible win conditions and keep checking if one is true   
 
 # Announce winner or tie
